07a07f152ee4f999e6442de52c1b125fbfd16e60
Gokul G.K <gokulgk1200@gmail.com>
2024-02-17 17:23:09 +0530

    Adapter design pattern

---
+++ DesignPatterns/src/main/java/CreationalDesignPattern/Adapter/round/RoundHole.java
@@ -0,0 +1,41 @@
+package CreationalDesignPattern.Adapter.round;
+
+/**
+ * The type Round hole.
+ */
+public class RoundHole {
+    /**
+     * The Radius.
+     */
+    private double radius;
+
+    /**
+     * Instantiates a new Round hole.
+     *
+     * @param radius the radius
+     */
+    public RoundHole(double radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * Gets radius.
+     *
+     * @return the radius
+     */
+    public double getRadius() {
+        return radius;
+    }
+
+    /**
+     * Fits boolean.
+     *
+     * @param peg the peg
+     * @return the boolean
+     */
+    public boolean fits(RoundPeg peg) {
+        boolean result;
+        result = (this.getRadius() >= peg.getRadius());
+        return result;
+    }
+}
