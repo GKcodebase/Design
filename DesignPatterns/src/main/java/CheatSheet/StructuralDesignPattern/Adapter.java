719474f9405e349661755a5f24cb15bfc564f574
Gokul G.K <gokulgk1200@gmail.com>
2024-12-23 16:57:04 -0600

    Create Adapter.java

---
+++ DesignPatterns/src/main/java/CheatSheet/StructuralDesignPattern/Adapter.java
@@ -0,0 +1,102 @@
+package CheatSheet.StructuralDesignPattern;
+
+/**
+ * The Adapter.
+ */
+public class Adapter {
+    /**
+     * The interface Media player.
+     */
+// Existing interface
+    interface MediaPlayer {
+        /**
+         * Play.
+         *
+         * @param audioTYpe the audio t ype
+         * @param fileName  the file name
+         */
+        void play(String audioTYpe, String fileName);
+    }
+
+    /**
+     * The interface Advanced media player.
+     */
+// Advanced media player
+    interface AdvancedMediaPlayer {
+        /**
+         * Play vlc.
+         *
+         * @param fileName the file name
+         */
+        void playVlc(String fileName);
+
+        /**
+         * Play mp 4.
+         *
+         * @param fileName the file name
+         */
+        void playMp4(String fileName);
+    }
+
+    /**
+     * The type Vlc media player.
+     */
+//Concrete implementation of advanced Media player
+    class VlcMediaPlayer implements AdvancedMediaPlayer {
+
+        /**
+         * Play vlc.
+         *
+         * @param fileName the file name
+         */
+        @Override
+        public void playVlc(String fileName) {
+            //impl
+        }
+
+        /**
+         * Play mp 4.
+         *
+         * @param fileName the file name
+         */
+        @Override
+        public void playMp4(String fileName) {
+            //impl
+        }
+    }
+
+    /**
+     * The type Media adapter.
+     */
+//Adapter Class
+    class MediaAdapter implements MediaPlayer {
+        /**
+         * The Advanced music player.
+         */
+        AdvancedMediaPlayer advancedMusicPlayer;
+
+        /**
+         * Instantiates a new Media adapter.
+         *
+         * @param audioType the audio type
+         */
+        public MediaAdapter(String audioType) {
+            if (audioType.equalsIgnoreCase("vlc")) {
+                advancedMusicPlayer = new VlcMediaPlayer();
+            }
+        }
+
+        /**
+         * Play.
+         *
+         * @param audioType the audio type
+         * @param fileName  the file name
+         */
+        @Override
+        public void play(String audioType, String fileName) {
+            if (audioType.equalsIgnoreCase("vlc")) {
+                advancedMusicPlayer.playVlc(fileName);
+            }
+        }
+    }
+}
