5bef6a03f525e0a670fb21480b540ea52330777b
Gokul G.K <gokulgk1200@gmail.com>
2026-01-12 14:45:29 +0530

    Update OpenClosePrinciple.java

--- ObjectOrientedDesign/src/main/java/SOLID/OpenClosePrinciple.java
+++ ObjectOrientedDesign/src/main/java/SOLID/OpenClosePrinciple.java
@@ -8,16 +8,48 @@ public class OpenClosePrinciple {
         public double area(double length, double width) {
             return length * width;
         }
-
-        public abstract double area(double side);
     }
-
-    // Extending Rectangle without modifying it
+    // Extending Rectangle  and  modifying are
     class Square extends Rectangle {
-        @Override
         public double area(double side) {
             return side * side;
         }
     }
 
+    // OCP Strategy  Use proper abstraction (most common & clean)
+    interface Shape {
+        double area();           // closed for modification
+    }
+
+    // Now we can add any new shape without touching existing code
+    class Rectangleocp implements Shape {
+        private double length;
+        private double width;
+
+        public Rectangleocp(double length, double width) {
+            this.length = length;
+            this.width = width;
+        }
+
+        @Override
+        public double area() {
+            return length * width;
+        }
+    }
+
+    class Squareocp implements Shape {
+        private double side;
+
+        public Squareocp(double side) {
+            this.side = side;
+        }
+
+        @Override
+        public double area() {
+            return side * side;
+        }
+    }
+
 }
+
+
